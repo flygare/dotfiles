675cb23f84f1d5141d33a94c8bbe0237c2d71da5
Robin Flygare <robin.flygare1@gmail.com>
2017-08-14 14:06:01 +0200

    Add travis (#3)
    * Add travis
    
    * Update README.md
    
    * Update dotfiles script
    
    * Set branch in test to travis branch

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 ######## Variables
-
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
 files=".gitconfig.local .oh-my-zsh/custom/themes .gitconfig .vimrc .zshrc"        # list of files/folders to symlink in homedir
@@ -9,120 +8,171 @@ gitname=""
 gitemail=""
 
 ######## Functions
-. "utils.sh" || { echo "Could not find utils.sh"; exit 1; }
-
-function setup-git {
-install-git
-check-dotfiles
+function install-generic {
+  pkg-install git
+  check-dotfiles
+  setup-gitconfig
+  print_header "Installs"
+  pkg-install curl
+  pkg-install zsh
+  install-ohmyzsh &> /dev/null
+  symlink
 }
 
-function install-git {
-if [[ ! $(which git 2>/dev/null) && "$OSTYPE" == "linux-gnu" ]]; then
-  sudo apt-get install git
-elif [ ! $(which git 2>/dev/null) ]; then
+function linux {
+  print_header "Linux detected, starting installation"
+  install-generic
+  print_header_result $? "Installation complete"
+}
+
+function macos {
+  print_header "Mac detected, starting installation"
   install-brew
-  brew install git
+  install-generic
+  pkg-install autojump
+  print_header_result $? "Installation complete"
+}
+
+function pkg-install {  
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+  # Linux install
+  sudo apt-get install $1 &> /dev/null
+  print_result $? "$1"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  # Mac OSX install
+  brew install $1 &> /dev/null
+  print_result $? "$1"
+else
+  print_error "Your operating system is not supported"
 fi
-print_result $? "Install git"
 }
 
 function check-dotfiles {
   if [ -d "$HOME/dotfiles/" ]; then
     print_warning "dotfiles already exist"
   else
-    git clone https://github.com/flygare/dotfiles.git ~/dotfiles
+    git clone https://github.com/flygare/dotfiles.git ~/dotfiles &> /dev/null
     print_result $? "Clone dotfiles"
   fi
 }
 
-function linux {
-  setup-git
-  setup-gitconfig
-    symlink
-    print_header "Installs"
-    linux-install-curl
-    linux-install-zsh
-}
-
-function macos {
-  setup-git
-  setup-gitconfig
-    symlink
-    print_header "Installs"
-    install-brew
-    brew install zsh
-    print_result $? "Zsh"
-    brew install autojump
-    print_result $? "Autojump"
-}
-
-function linux-install-curl {
-  if [ ! $(which curl 2>/dev/null) ]; then
-    sudo apt-get install curl
-      fi
-      print_result $? "Curl"
-}
-
-function linux-install-zsh {
-  if [ ! $(which curl 2>/dev/null) ]; then
-    sudo apt-get install zsh
-      fi
-      print_result $? "Zsh"
-}
-
 function install-brew {
   if [ ! $(which brew 2>/dev/null) ]; then
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-      fi
-      print_result $? "Homebrew"
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" &> /dev/null
+  fi
+  print_result $? "Homebrew"
 }
 
 function install-ohmyzsh {
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-    print_result $? "Oh-my-zsh"
+  print_result $? "Oh-my-zsh"
 }
 
 function symlink {
   print_header "Symlinks"
-    mkdir -p $olddir
-    cd $dir
-    for file in $files; do
+  mkdir -p $olddir
+  cd $dir
+  for file in $files; do
+    if [ -e "$HOME/$file" ]; then
       mv ~/$file ~/dotfiles_old/
-        done
-        print_result $? "Back up any existing dotfiles from ~ to $olddir"
+    fi
+  done
+  print_result $? "Back up any existing dotfiles from ~ to $olddir"
 
-        for file in $files; do
-          ln -s $dir/$file ~/$file
-            done
-            print_result $? "Symlinking dotfiles"
+  for file in $files; do
+    ln -s $dir/$file ~/$file
+  done
+  print_result $? "Symlinking dotfiles"
 }
 
 function setup-gitconfig {
   print_header "Set up gitconfig"
-    if [ ! -f .gitconfig.local ]; then
-      print_warning ".gitconfig.local not found, prompting for git settings"
-        echo "Enter name: "
-        read -r gitname
-        echo "Enter email: "
-        read -r gitemail
+  if [ ! -f .gitconfig.local ]; then
+    print_warning ".gitconfig.local not found, prompting for git settings"
+    echo "Enter name: "
+    read -r gitname
+    echo "Enter email: "
+    read -r gitemail
 
-        printf "[user]\n  email=$gitemail\n  name=$gitname\n" > .gitconfig.local
-        fi
-        print_result $? "Local gitconfig"
+    printf "[user]\n  email=$gitemail\n  name=$gitname\n" > .gitconfig.local
+  fi
+  print_result $? "Local gitconfig"
+}
+
+######## Color helpers
+print_error() {
+  print_in_red "   [✖] $1 $2\n"
+}
+
+print_error_stream() {
+  while read -r line; do
+    print_error "↳ ERROR: $line"
+  done
+}
+
+print_header() {
+  print_in_purple "\n • $1\n\n"
+}
+
+print_header_result() {
+  if [ "$1" -eq 0 ]; then
+    print_in_green "\n ✔ $2\n\n"
+  else
+    print_in_red "\n ✖ $2\n\n"
+  fi
+  return "$1"
+}
+
+print_subheader() {
+  print_in_purple "   $1\n\n"
+}
+
+print_in_color() {
+  printf "%b" \
+    "$(tput setaf "$2" 2> /dev/null)" \
+    "$1" \
+    "$(tput sgr0 2> /dev/null)"
+}
+
+print_in_green() {
+  print_in_color "$1" 2
+}
+
+print_in_purple() {
+  print_in_color "$1" 5
+}
+
+print_in_red() {
+  print_in_color "$1" 1
+}
+
+print_in_yellow() {
+  print_in_color "$1" 3
+}
+
+print_warning() {
+  print_in_yellow "   [!] $1\n"
+}
+
+print_result() {
+  if [ "$1" -eq 0 ]; then
+    print_success "$2"
+  else
+    print_error "$2"
+  fi
+  return "$1"
+}
+
+print_success() {
+  print_in_green "   [✔] $1\n"
 }
 
 ########## Main
 # Check operating system
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-# Linux install
-print_warning "Linux detected, starting installation"
-linux
-print_header_result $? "Installation complete"
+  linux
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-# Mac OSX install
-print_warning "Mac detected, starting installation"
-macos
-print_header_result $? "Installation complete"
+  macos
 else
-print_error "Your operating system is not supported"
+  print_error "Your operating system is not supported"
 fi
